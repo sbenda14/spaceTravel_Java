ff1c793d9314abdf07c91dc844c258e3936fd29c
sbenda14 <susanbenda@u.boisestate.edu>
2021-02-01 14:19:11 -0700

    results panel

--- SpaceTravel/source/gui/GUI.java
+++ SpaceTravel/source/gui/GUI.java
@@ -1,12 +1,19 @@
 package gui;
 
-import javax.swing.JFileChooser;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
+
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.io.File;
+import java.awt.Toolkit;
 
 /**
  * GUI panel for project. 
@@ -19,25 +26,41 @@ public class GUI extends JFrame {
     * Create the frame.
     */
    public GUI() {
-	   setLayout(new FlowLayout());
 	   setTitle("Space Travel");
-	// set up a file picker component
-       FilePicker filePicker = new FilePicker("Pick a file", "Browse...");
-       filePicker.setMode(FilePicker.MODE_OPEN);
-       filePicker.addFileTypeFilter(".txt", "Text files");
-        
-       // access JFileChooser class directly
-       JFileChooser fileChooser = filePicker.getFileChooser();
-       fileChooser.setCurrentDirectory(new File("C:/Users/sbend/OneDrive/Desktop"));
-        
-       // add the component to the frame
-       setVisible(true);
-	   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       setBounds(100, 100, 450, 300);
-       contentPane = new JPanel();
-       contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-       contentPane.setLayout(new BorderLayout(0, 0));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(GUI.class.getResource("/gui/favicon-16x16.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 345);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		
+		// set up a file picker component
+	    FilePicker filePicker = new FilePicker("Pick a file", "Browse...");
+	    filePicker.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+	    FlowLayout flowLayout = (FlowLayout) filePicker.getLayout();
+        filePicker.setMode(FilePicker.MODE_OPEN);
+        filePicker.addFileTypeFilter(".txt", "Text files");
+        filePicker.setSize(new Dimension(345, 5));
+       
+	        
        contentPane.add(filePicker);
-       setContentPane(contentPane);
+		
+        JPanel runAlgo = new JPanel();
+        runAlgo.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		runAlgo.setLayout(new BorderLayout(0, 0));
+		JButton btnNearestNeighbor = new JButton("Run Nearest Neighbor");
+		runAlgo.add(btnNearestNeighbor, BorderLayout.NORTH);
+		
+		JLabel lblDistance = new JLabel("Total Distance:");
+		runAlgo.add(lblDistance, BorderLayout.SOUTH);
+		
+		JLabel lblResults = new JLabel("Results");
+		lblResults.setOpaque(true);
+		lblResults.setBackground(Color.WHITE);
+		lblResults.setVerticalAlignment(SwingConstants.TOP);
+		runAlgo.add(lblResults, BorderLayout.CENTER);
+		contentPane.add(runAlgo);
    }
+   
 }
