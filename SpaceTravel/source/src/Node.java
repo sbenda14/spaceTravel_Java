0952d1b4ebe589818704e92b8f52d6d637bf61ec
sbenda14 <susanbenda@u.boisestate.edu>
2021-02-06 11:12:57 -0700

    comment code

--- SpaceTravel/source/src/Node.java
+++ SpaceTravel/source/src/Node.java
@@ -1,5 +1,5 @@
 package src;
-/*
+/**
 * Node class. Holds a GPS point name and its XYZ coordinates	
 */
 
@@ -10,7 +10,7 @@ public class Node {
 	private double Z;
 	private boolean start;
 	
-	 /* Constructor for waypoint, assigns name, XYZ coordinates, and indicates start point based on five parameters
+	 /** Constructor for waypoint, assigns name, XYZ coordinates, and indicates start point based on five parameters
      * @param point, waypoint name
      * @param Xpt, the X coordinate
      * @param Ypt, the Y coordinate
@@ -25,26 +25,50 @@ public class Node {
     	this.start = startpt;
     }
     
+    /**
+     * Returns a string of waypoints name
+     * @return waypoint name
+     */
     public String getWaypoint() {
     	return waypoint;
     }
     
+    /**
+     * Returns X coordinate of waypoint
+     * @return X
+     */
     public double getX() {
     	return X;
     }
     
+    /**
+     * Returns Y coordinate of waypoint
+     * @return Y 
+     */
     public double getY() {
     	return Y;
     }
     
+    /**
+     * Returns Z coordinate of waypoint
+     * @return Z 
+     */
     public double getZ() {
     	return Z;
     }
     
+    /**
+     * Returns true if waypoint is the starting point
+     * @return True if starting point
+     */
     public boolean isStart() {
     	return start;
     }
     
+    /**
+     * Formats and returns string of Waypoint Name, XYZ coordinates
+     * @return waypoint toString
+     */
     public String toString(){
         return "[" + this.waypoint + " => " + this.X +":" + this.Y +":" + this.Z + "]";
     }
